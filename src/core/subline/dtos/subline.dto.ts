c2fa3c65da350c3762d0a86daceea2d858e600eb
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-06-05 21:52:22 +0700

    fix : fix subline dto

--- src/core/subline/dtos/subline.dto.ts
+++ src/core/subline/dtos/subline.dto.ts
@@ -1,21 +1,35 @@
 import { Subtitle } from 'src/core/subtitle/models';
-import { IsDate, IsUUID } from 'class-validator';
+import { IsDate, IsNotEmpty, IsUUID } from 'class-validator';
 import { CheckString } from 'src/decorators';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SubLineDTO {
+  @ApiProperty({ description: 'Id', type: String })
   @IsUUID()
   id: string;
 
+  @ApiProperty({ description: 'Subtitle of Subline' })
   subtitle: Subtitle;
 
   // @ApiProperty({ default: '' })
   // @IsString()
   // @IsNotEmpty()
   // supporterId: string;
-
+  @ApiProperty({
+    description: 'Timestamp',
+    required: true,
+    default: new Date(Date.now()).toISOString(),
+  })
   @IsDate()
+  @IsNotEmpty()
   timestamp: Date;
 
+  @ApiProperty({
+    description: 'Content',
+    type: String,
+    required: true,
+    default: '',
+  })
   @CheckString()
   content: string;
 }
