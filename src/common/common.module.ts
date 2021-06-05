c49a2425c1592629da375e46f44cce1624e82af2
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-06-05 21:20:02 +0700

    fix : fix

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,5 +1,6 @@
 import { Global, Module } from '@nestjs/common';
+import { AppConfigModule } from './config';
 
 @Global()
-@Module({ imports: [] })
+@Module({ imports: [AppConfigModule] })
 export class CommonModule {}
