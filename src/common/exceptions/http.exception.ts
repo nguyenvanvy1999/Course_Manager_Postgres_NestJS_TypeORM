a4f14d23aaecc6c0ca831016d752de29ab226d2e
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-06-08 15:22:49 +0700

    fix : fix exception

--- src/common/exceptions/http.exception.ts
+++ src/common/exceptions/http.exception.ts
@@ -8,6 +8,8 @@ import {
 import { AppLogger } from '../logger';
 import { Response, Request } from 'express';
 import { ErrorRes } from './dtos';
+import { configService } from '../config';
+
 @Catch()
 export class HttpExceptionsFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
@@ -19,11 +21,26 @@ export class HttpExceptionsFilter implements ExceptionFilter {
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
+    if (
+      status === HttpStatus.UNAUTHORIZED &&
+      request.originalUrl === '/auth/profile'
+    ) {
+      response.setHeader(
+        'Access-Control-Allow-Origin',
+        configService.getClientUrl(),
+      );
+      response.setHeader(
+        'Set-Cookie',
+        `Authentication=; HttpOnly; Path=/; Max-Age=0;SameSite=None; Secure`,
+      );
+      response.status(HttpStatus.OK).send();
+      return;
+    }
     let message: any =
       exception instanceof HttpException
         ? exception.getResponse()
         : 'Internal server';
-    message = message instanceof Object ? message?.message : message;
+    message = message instanceof Object ? message.message : message;
     if (message instanceof Array) {
       for (let i = 0; i < message.length; i++) {
         if (message[i].split(' ')[0].includes('.')) {
@@ -33,6 +50,10 @@ export class HttpExceptionsFilter implements ExceptionFilter {
         }
       }
     }
+    response.setHeader(
+      'Access-Control-Allow-Origin',
+      configService.getClientUrl(),
+    );
     const res: ErrorRes = {
       status,
       timestamp: new Date().toLocaleString(),
